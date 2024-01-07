037be9bb8a820500b69eb3b211fe57ad342d1407
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-01-08 00:59:45 +0300

    Add config file reading

--- src/main/java/logalyzes/server/Main.java
+++ src/main/java/logalyzes/server/Main.java
@@ -1,9 +1,13 @@
 package logalyzes.server;
 
 import logalyzes.server.App;
+import logalyzes.server.utils.Config;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Reading env variables from .env file
+        Config config = new Config(".env");
         App app = new App();
         app.start();
     }
