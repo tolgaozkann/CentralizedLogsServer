0b2f6aa90b5a88c18368629bc80836713d78bc24
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-01-20 19:47:39 +0300

    Add getFullStringDate method to DateUtils

--- src/main/java/logalyzes/server/utils/DateUtils.java
+++ src/main/java/logalyzes/server/utils/DateUtils.java
@@ -1,14 +1,20 @@
 package logalyzes.server.utils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
-
 
     public  static String getStringDate() {
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return date.format(formatter);
+    }
+
+    public static String getFullStringDate() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dateTime.format(formatter);
     }
 }
