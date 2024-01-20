c99e91e75dd745f96c8c1abfc70529901bd61a96
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-01-20 19:47:47 +0300

    Refactor Mapper class to extend ObjectMapper
    This commit refactors the Mapper class to extend the ObjectMapper class from the com.fasterxml.jackson.databind package. It also adds a new method getInstance() to retrieve a singleton instance of the Mapper class. The disable(SerializationFeature.FAIL_ON_EMPTY_BEANS) method is called in the constructor to disable serialization failure on empty beans.

--- src/main/java/logalyzes/server/utils/Mapper.java
+++ src/main/java/logalyzes/server/utils/Mapper.java
@@ -1,4 +1,21 @@
 package logalyzes.server.utils;
 
-public class Mapper {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class Mapper extends ObjectMapper {
+
+    private static Mapper instance = null;
+
+    private Mapper() {
+        super();
+        this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    public static Mapper getInstance() {
+        if(instance == null) {
+            instance = new Mapper();
+        }
+        return instance;
+    }
 }
