61762b07935aaafb6cfb4842ef19a032c1ad30d4
SametHaymana <abdulsamethaymana@gmail.com>
2024-05-14 15:24:25 +0300

    Update dev script

--- mvnmoon.sh
+++ mvnmoon.sh
@@ -1,68 +1,56 @@
 #!/bin/bash
-##
-# This script used for make development environment used for faster development,
-#
-##
 
 WATCH_DIR="src/"
-
-DEBOUNCE_DELAY=1
+DEBOUNCE_DELAY=2
 MAVEN_BUILD="mvn clean install"
-
 RUN_APP="java -jar target/LogServer-1.0-SNAPSHOT.jar"
-
 APP_PID=0
 
-
-
-## Check inotifywait is installed or not
+## Check if inotifywait is installed
 if ! [ -x "$(command -v inotifywait)" ]; then
-  echo "sudo apt install inotify-tools"
+  echo "inotify-tools is not installed. Installing now..."
+  sudo apt-get install -y inotify-tools
 fi
 
-
-function start() {
-    echo "Starting App"
+function start_app() {
+    echo "Starting App..."
     $RUN_APP &
     APP_PID=$!
-    echo "Application started with PID $APP_PID"
+    echo "Application started with PID: $APP_PID"
 }
 
-function stop() {
+function stop_app() {
     if [ $APP_PID -ne 0 ]; then
-        echo "Stopping application with PID $APP_PID"
+        echo "Stopping application with PID: $APP_PID"
         kill $APP_PID
         wait $APP_PID 2>/dev/null
         APP_PID=0
     fi
 }
 
-
-
-function clean() {
-    echo "Exiting..."
-    stop
-    exit 0
-}
-
-rebuild_and_restart() {
-    stop
+function rebuild_and_restart() {
+    stop_app
+    echo "Building the project..."
     $MAVEN_BUILD
-    start
+    start_app
 }
 
-trap clean SIGINT
+trap 'stop_app; echo "Script terminated"; exit' SIGINT SIGTERM
 
-# Initial build and start
+echo "Initial build and start..."
 $MAVEN_BUILD
-start
+start_app
 
-# Watch for changes in the directory
+# Watch and debounce changes
+LAST_BUILD_TIME=0
 while true; do
-    inotifywait -r -e modify,create,delete --format '%w%f' -qq $WATCH_DIR
-    echo "Change detected. Rebuilding and restarting in $DEBOUNCE_DELAY seconds..."
-    sleep $DEBOUNCE_DELAY
-    rebuild_and_restart
+    FILE_CHANGED=$(inotifywait -r -e modify,create,delete --format '%w%f' -qq $WATCH_DIR)
+    CURRENT_TIME=$(date +%s)
+
+    if [[ $((CURRENT_TIME - LAST_BUILD_TIME)) -gt $DEBOUNCE_DELAY ]]; then
+        echo "Change detected in $FILE_CHANGED. Rebuilding and restarting after $DEBOUNCE_DELAY seconds of inactivity..."
+        sleep $DEBOUNCE_DELAY
+        LAST_BUILD_TIME=$(date +%s)
+        rebuild_and_restart
+    fi
 done
-
-
