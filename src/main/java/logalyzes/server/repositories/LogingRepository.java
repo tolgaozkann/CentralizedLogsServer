da4cda787af09e52d7aa08f15f2ea09e9d5c8394
SametHaymana <abdulsamethaymana@gmail.com>
2024-05-20 17:50:55 +0300

    Refactore clean file

--- src/main/java/logalyzes/server/repositories/LogingRepository.java
+++ src/main/java/logalyzes/server/repositories/LogingRepository.java
@@ -1,27 +1,12 @@
 package logalyzes.server.repositories;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
-import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
-import co.elastic.clients.elasticsearch.cat.IndicesResponse;
-import co.elastic.clients.elasticsearch.cat.indices.IndicesRecord;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.indices.IndicesStatsResponse;
-import co.elastic.clients.json.JsonData;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.util.JsonFormat;
-import com.logalyzes.logs.dtos.*;
 import logalyzes.server.core.ElkCore;
 import logalyzes.server.dtos.LogDto;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -35,15 +20,15 @@ public class LogingRepository {
         client = ElkCore.getInstance().getClient();
     }
 
-    public  CompletableFuture<ListIndexesResponse> listIndexes()  {
+    public  CompletableFuture<com.logalyzes.logs.dtos.ListIndexesResponse> listIndexes()  {
         CompletableFuture<IndicesStatsResponse> indicesStatsFuture = client.indices().stats();
 
         return indicesStatsFuture.thenApply(indicesStatsResponse -> {
             // Map each entry to an Index object using a builder and collect into an ArrayList
-            ArrayList<Index> indexes = indicesStatsResponse.indices().entrySet().stream().map(entry -> {
+            ArrayList<com.logalyzes.logs.dtos.Index> indexes = indicesStatsResponse.indices().entrySet().stream().map(entry -> {
                         // Ensure that total and docs are not null before accessing
                         if (entry.getValue().total() != null && entry.getValue().total().docs() != null) {
-                            return Index.newBuilder()
+                            return com.logalyzes.logs.dtos.Index.newBuilder()
                                     .setName(entry.getKey())
                                     .setSize(entry.getValue().total().docs().count())
                                     .build();
@@ -54,7 +39,7 @@ public class LogingRepository {
                     .filter(index -> index != null)  // Remove null entries from the list
                     .collect(Collectors.toCollection(ArrayList::new));
 
-            ListIndexesResponse listIndexesResponse = ListIndexesResponse.newBuilder()
+            com.logalyzes.logs.dtos.ListIndexesResponse listIndexesResponse = com.logalyzes.logs.dtos.ListIndexesResponse.newBuilder()
                     .addAllIndexes(indexes)
                     .build();
 
@@ -63,7 +48,7 @@ public class LogingRepository {
     }
 
 
-    public CompletableFuture<LogsResponse> list(LogsRequest request) throws Exception {
+    public CompletableFuture<com.logalyzes.logs.dtos.LogsResponse> list(com.logalyzes.logs.dtos.LogsRequest request) throws Exception {
         SearchRequest.Builder query = new SearchRequest.Builder()
                 .index(request.getIndex())
                 .from((request.getPage() - 1) * request.getPageSize())
@@ -82,11 +67,11 @@ public class LogingRepository {
                 query.build(),
                 LogDto.class
         ).thenApply(response -> {
-            List<LogsMessages.Log> logs =  response.hits().hits().stream().map( hit ->
+            List<com.logalyzes.logs.dtos.LogsMessages.Log> logs =  response.hits().hits().stream().map(hit ->
                 hit.source().toLog()
             ).collect(Collectors.toList());
 
-            return LogsResponse.newBuilder()
+            return com.logalyzes.logs.dtos.LogsResponse.newBuilder()
                     .addAllLogs(logs)
                     .setPage(request.getPage())
                     .setPageSize(request.getPageSize())
@@ -97,7 +82,7 @@ public class LogingRepository {
         });
     }
 
-    public CompletableFuture<LogsMessages.Log> details(String id) throws Exception {
+    public CompletableFuture<com.logalyzes.logs.dtos.LogsMessages.Log> details(String id) throws Exception {
        return  null;
     }
 
