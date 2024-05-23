68d9179cba69893607a603a466e81f37df2f2eba
SametHaymana <abdulsamethaymana@gmail.com>
2024-05-23 16:50:56 +0300

    Applying after processing log message

--- src/main/java/logalyzes/server/controllers/LogCollectorService.java
+++ src/main/java/logalyzes/server/controllers/LogCollectorService.java
@@ -7,6 +7,9 @@ import com.logalyzes.logs.dtos.LogCollectorServiceGrpc.LogCollectorServiceImplBa
 import  com.logalyzes.logs.dtos.LogsMessages.*;
 import io.grpc.Status;
 import io.grpc.stub.*;
+import logalyzes.server.core.NotificationProcessor;
+import logalyzes.server.core.NotificationTask;
+import logalyzes.server.core.Predictor;
 import logalyzes.server.dtos.LogDto;
 import logalyzes.server.repositories.LogCollectorRepository;
 import logalyzes.server.utils.logger.LOG_LEVEL;
@@ -18,11 +21,18 @@ import java.util.concurrent.CompletableFuture;
 public class LogCollectorService extends LogCollectorServiceImplBase {
     private LogCollectorRepository repo;
     private  Logger logger = null;
+    private Predictor predictor;
+
+    // Notification processor
+    NotificationProcessor processor;
 
 
     public LogCollectorService() {
         this.repo = new LogCollectorRepository();
         this.logger = Logger.getInstance();
+        this.predictor = new Predictor();
+
+        this.processor = new NotificationProcessor(3);
     }
 
     @Override
@@ -30,14 +40,29 @@ public class LogCollectorService extends LogCollectorServiceImplBase {
             LogForCreate request,
             StreamObserver<LogCreatedResponse> responseObserver)  {
 
+
          CompletableFuture.runAsync(() -> {
             try {
                 // Did not wait for the response
                 responseObserver.onNext(LogCreatedResponse.newBuilder().setCreated(true).build());
                 responseObserver.onCompleted();
 
+
+                LogDto dto = new LogDto(request);
                 // Process the message
-                this.repo.save(new LogDto(request));
+                this.repo.save(dto);
+
+
+                // Handle and send request to prediction
+                String log =  request.getApplication().getName()
+                        + request.getApplication().getVersion()
+                        + request.getMessage()
+                        + request.getStackTrace();
+
+                int[] res = predictor.sendLog(log);
+
+                // Process output with async
+                this.processor.queueTask(new NotificationTask(dto.getId(),res));
 
             } catch (Exception e) {
                 String msg = "Error while processing message";
@@ -46,6 +71,8 @@ public class LogCollectorService extends LogCollectorServiceImplBase {
                 throw new RuntimeException(e);
             }
         });
+
+
 
     }
 
