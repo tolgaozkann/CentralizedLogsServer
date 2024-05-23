c50ee6b854eed4fe7e51e0f3d739fcc9ef6dcccc
SametHaymana <abdulsamethaymana@gmail.com>
2024-05-23 16:49:40 +0300

    Refactore enum

--- src/main/java/logalyzes/server/utils/logger/LOG_LEVEL.java
+++ src/main/java/logalyzes/server/utils/logger/LOG_LEVEL.java
@@ -1,9 +1,33 @@
 package logalyzes.server.utils.logger;
 
 public enum LOG_LEVEL {
-    DEBUG ,
-    INFO ,
-    WARN ,
-    ERROR ,
-    FATAL ,
+    DEBUG(0),
+    INFO(1),
+    WARN(2),
+    ERROR(3),
+    FATAL(4);
+
+    private final int value;
+
+    LOG_LEVEL(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static LOG_LEVEL fromInt(int i) {
+        for (LOG_LEVEL level : LOG_LEVEL.values()) {
+            if (level.getValue() == i) {
+                return level;
+            }
+        }
+        throw new IllegalArgumentException("Invalid LOG_LEVEL value: " + i);
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
